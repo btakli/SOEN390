7ac1e8f5a7bce4a599519ed173eae10a15098d95
btakli <64867481+btakli@users.noreply.github.com>
2022-01-26 16:11:13 -0500

    Added test for the Person Model.
    Note: test_models is duplicated in \backend and \tests\integration tests because I could not get "from backend.companion_api.models import Person" working.
    
    If someone has an idea that'd be helpful.

---
+++ backend/test_models.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from companion_api.models import Person
+
+import datetime
+
+class PersonTestCase(TestCase):
+    """This is a test case which tests that the attributes of the Person Model are properly set up.
+    """
+    def setUp(self):
+        Person.objects.create(first_name="Brandon", last_name="Takli", email = "brandon@hotmail.com", date_of_birth = datetime.date(day=20, month=10, year=2000))
+        Person.objects.create(first_name="John", last_name="Smith", email = "john@gmail.com", date_of_birth = datetime.date(day=2, month=1, year=2001))
+    def test_person_attribute(self):
+        """Animals that can speak are correctly identified"""
+        p1 = Person.objects.get(first_name="Brandon")
+        p2 = Person.objects.get(date_of_birth=datetime.date(day=2, month=1, year=2001))
+        p3 = Person.objects.get(last_name="Smith")
+        p4 = Person.objects.get(email="brandon@hotmail.com")
+        self.assertEqual(p1.last_name, 'Takli')
+        self.assertEqual(p2, p3)
+        self.assertEqual(p1.email, "brandon@hotmail.com")
+        self.assertEqual(p1, p4)
+        
