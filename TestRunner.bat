b8408e81c8fc23b1896a4ea8328e79991e8b85db
Matteo Gisondi <matteo.gisondi@gmail.com>
2022-02-01 15:15:04 -0500

    Added backend linting, metrics + frontend testing

--- TestRunner.bat
+++ TestRunner.bat
@@ -1,19 +1,57 @@
-@ECHO OFF
-cd backend
-SET BACKENDREPORT=BackendTestReport.txt
-SET BACKENDCOVERAGE=BackendTestCoverage.txt
-ECHO "~~~~~~~~~~~~~~~~~~~Running backend tests and placing report in SOEN390/Reports/%BACKENDREPORT%...~~~~~~~~~~~~~~~~~~~"
-coverage run --branch --source='.' manage.py test tests -v 2 >> %BACKENDREPORT% 2>&1
-type %BACKENDREPORT%   
-ECHO "~~~~~~~~~~~~~~~~~~~Generating coverage report and placing it in SOEN390/Reports/%BACKENDCOVERAGE%...~~~~~~~~~~~~~~~~~~~"
+@echo OFF
 
-coverage report -m  --omit="*/test*" >> %BACKENDCOVERAGE% 
+echo ~~~~~~~~~~~~~~~~~~~Backend~~~~~~~~~~~~~~~~~~~
+cd backend
+set BACKENDREPORT=BackendTestReport.txt
+set BACKENDCOVERAGE=BackendTestCoverage.txt
+set BACKENDSTYLE=BackendStyle.txt
+set BACKENDMETRICS=BackendMetrics.txt
+
+echo ~~~~~~~~~~~~~~~~~~~Running tests~~~~~~~~~~~~~~~~~~~
+coverage run --branch --source='.' manage.py test tests -v 2 >> %BACKENDREPORT% 2>&1
+type %BACKENDREPORT%
+
+echo ~~~~~~~~~~~~~~~~~~~Generating coverage report~~~~~~~~~~~~~~~~~~~
+coverage report -m  --omit="*/test*" >> %BACKENDCOVERAGE%
 coverage html --omit="*/test*"
 type %BACKENDCOVERAGE%
+
+echo ~~~~~~~~~~~~~~~~~~~Generating linting report~~~~~~~~~~~~~~~~~~~
+pylint companion_api >> %BACKENDSTYLE%
+type %BACKENDSTYLE%
+
+echo ~~~~~~~~~~~~~~~~~~~Generating metric report~~~~~~~~~~~~~~~~~~~
+echo Raw Metrics >> %BACKENDMETRICS%
+radon raw companion_api >> %BACKENDMETRICS%
+echo; >> %BACKENDMETRICS%
+echo Clomatic Complexity >> %BACKENDMETRICS%
+radon cc companion_api >> %BACKENDMETRICS%
+echo; >> %BACKENDMETRICS%
+echo Maintainability Index >> %BACKENDMETRICS%
+radon mi companion_api >> %BACKENDMETRICS%
+type %BACKENDMETRICS%
+
+echo ~~~~~~~~~~~~~~~~~~~Migrating reports to tests/Reports~~~~~~~~~~~~~~~~~~~
 if not exist "./tests/Reports" mkdir "./tests/Reports"
 move /y %BACKENDREPORT% ./tests/Reports
 move /y %BACKENDCOVERAGE% ./tests/Reports
 if exist "./tests/Reports/htmlcov" rmdir /s /q "./tests/Reports/htmlcov"
 move /y htmlcov ./tests/Reports
-ECHO "~~~~~~~~~~~~~~~~~~~Done!~~~~~~~~~~~~~~~~~~~"
-PAUSE
+move /y %BACKENDSTYLE% ./tests/Reports
+move /y %BACKENDMETRICS% ./tests/Reports
+cd ..
+
+echo ~~~~~~~~~~~~~~~~~~~Frontend~~~~~~~~~~~~~~~~~~~
+cd frontend2
+set FRONTENDREPORT=FrontendTestReport.txt
+
+echo ~~~~~~~~~~~~~~~~~~~Running tests~~~~~~~~~~~~~~~~~~~
+call npx jest -i --ci --coverage >> %FRONTENDREPORT%
+
+echo ~~~~~~~~~~~~~~~~~~~Migrating reports to tests/Reports~~~~~~~~~~~~~~~~~~~
+if not exist "./tests/Reports" mkdir "./tests/Reports"
+move /y %FRONTENDREPORT% ./tests/Reports
+cd ..
+
+echo ~~~~~~~~~~~~~~~~~~~Done!~~~~~~~~~~~~~~~~~~~
+pause
