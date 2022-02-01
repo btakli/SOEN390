da7297bdb183443d00339e43fdbddd1b65e41c8b
Daniel Macicasan <danimacicasan@gmail.com>
2022-01-31 22:39:19 -0500

    Added the Sign Up page to the frontend

--- frontend/src/RoutesManager.js
+++ frontend/src/RoutesManager.js
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import App from "./App";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
+import SignUp from "./Pages/SignUp";
 
 export default function RoutesManager() {
   return (
     <Routes>
       <Route exact path="/" element={<Login />}></Route>
       <Route exact path="/login" element={<Login />}></Route>
+      <Route exact path="/signup" element={<SignUp />}></Route>
       <Route exact path="/home" element={<Home />}></Route>
     </Routes>
   );
