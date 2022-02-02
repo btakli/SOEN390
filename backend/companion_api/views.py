8a3046097055424a0c7871ff9efa56334cd11fc9
Peter Delis <delispeter19@gmail.com>
2022-02-01 23:14:06 -0500

    Simplify Person views and add permission checking

--- backend/companion_api/views.py
+++ backend/companion_api/views.py
@@ -1,25 +1,28 @@
 '''Create your views here'''
 
-from rest_framework import mixins, viewsets
+from rest_framework import viewsets, permissions
 from companion_api.models import *
 from companion_api.serializers import *
 
 
-class CreateListRetrieveDestroyUpdateViewSet(
-    mixins.CreateModelMixin,
-    mixins.ListModelMixin,
-    mixins.RetrieveModelMixin,
-    mixins.DestroyModelMixin,
-    mixins.UpdateModelMixin,
-    viewsets.GenericViewSet,
-):
-    '''Inheritance class'''
+# class CreateListRetrieveDestroyUpdateViewSet(
+#     mixins.CreateModelMixin,
+#     mixins.ListModelMixin,
+#     mixins.RetrieveModelMixin,
+#     mixins.DestroyModelMixin,
+#     mixins.UpdateModelMixin,
+#     viewsets.GenericViewSet,
+# ):
+#     '''Inheritance class'''
 
-    pass
+#     pass
 
 
-class PersonView(CreateListRetrieveDestroyUpdateViewSet):
+class PersonView(viewsets.ModelViewSet):
     '''Person View'''
 
     queryset = Person.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
     serializer_class = PersonSerializer
