ba823fd60d1de8f6956429f2418259515f395580
btakli <64867481+btakli@users.noreply.github.com>
2022-02-20 14:56:57 -0500

    Added descriptive comments for all test classes/tests -These will show up in our test report. Useful for our sprint report.

--- backend/tests/test_accounts/test_doctor_views.py
+++ backend/tests/test_accounts/test_doctor_views.py
@@ -2,13 +2,15 @@ from .test_setup import TestSetUp
 from accounts.models import User
 
 class TestViews(TestSetUp):
-
+    """Test suite for views related to the Doctor"""
     # REGISTER TESTS
     def test_doctor_cannot_register_with_no_data(self):
+        """Registration: Doctor cannot register with no data"""
         res = self.client.post(self.register_doctor_url)
         self.assertEqual(res.status_code, 400)
 
     def test_doctor_can_register_with_correct_data(self):
+        """Registration: Doctor can register with correct data"""
         res = self.client.post(self.register_doctor_url, self.correct_doctor_data, format='json')
 
         self.assertTrue(User.objects.filter(email = self.correct_doctor_data['user']['email']).exists())
@@ -16,32 +18,38 @@ class TestViews(TestSetUp):
         self.assertEqual(res.status_code, 201)
 
     def test_doctor_cannot_register_with_no_user(self):
+        """Registration: Doctor cannot register with no User (every Doctor contains a User)"""
         res = self.client.post(self.register_doctor_url, self.no_user_doctor_data, format='json')
 
         self.assertEqual(res.status_code, 400)
 
     def test_doctor_cannot_register_with_no_first_name(self):
+        """Registration: Doctor cannot register with no first_name attribute"""
         res = self.client.post(self.register_doctor_url, self.no_first_name_doctor_data, format='json')
 
         self.assertEqual(res.status_code, 400)
 
     def test_doctor_cannot_register_with_no_email(self):
+        """Registration: Doctor cannot register with no email attribute"""
         res = self.client.post(self.register_doctor_url, self.no_email_doctor_data, format='json')
 
         self.assertEqual(res.status_code, 400)
 
     def test_doctor_cannot_register_with_no_password(self):
+        """Registration: Doctor cannot register with no password attribute"""
         res = self.client.post(self.register_doctor_url, self.no_password_doctor_data, format='json')
 
         self.assertEqual(res.status_code, 400)
 
     def test_doctor_cannot_register_with_invalid_email_format(self):
+        """Registration: Doctor cannot register with an invalid email format"""
         self.correct_doctor_data['user']['email'] = '123'
         res = self.client.post(self.register_doctor_url, self.correct_doctor_data, format='json')
 
         self.assertEqual(res.status_code, 400)
 
     def test_doctor_cannot_register_with_existing_email(self):
+        """Registration: Doctor cannot register with an existing email (emails are unique)"""
         self.client.post(self.register_doctor_url, self.default_doctor_data, format='json')
 
         self.correct_doctor_data['user']['email'] = self.default_user_data['email']
@@ -66,18 +74,21 @@ class TestViews(TestSetUp):
 
     # LOGIN TESTS
     def test_doctor_can_login_when_registered(self):
+        """Login: Doctor can login once registered"""
         self.client.post(self.register_doctor_url, self.correct_doctor_data, format='json')
         res = self.client.post(self.login_doctor_url, self.correct_doctor_data['user'], format='json')
 
         self.assertEqual(res.status_code, 200)
 
     def test_doctor_cannot_login_when_not_registered(self):
+        """Login: Doctor cannot login if not registered"""
         res = self.client.post(self.login_doctor_url, self.correct_doctor_data['user'], format='json')
 
         self.assertEqual(res.status_code, 400)
 
     # USER VIEW
     def test_return_doctor_valid_token_authorized(self):
+        """User View: Return a valid token for the Doctor when authorized"""
         res = self.client.post(self.register_doctor_url, self.correct_doctor_data, format='json')
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + res.data['token'])
         res = self.client.get(self.users_doctor_url)
@@ -85,11 +96,13 @@ class TestViews(TestSetUp):
         self.assertEqual(res.status_code, 200)
 
     def test_return_doctor_no_token_unauthorized(self):
+        """User View: If no token for the Doctor, we are unauthorized"""
         res = self.client.get(self.users_doctor_url)
 
         self.assertEqual(res.status_code, 401)
 
     def test_return_doctor_invalid_token_unauthorized(self):
+        """User View: Invalid token means Doctor is unauthorized"""
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + '123')
         res = self.client.get(self.users_doctor_url)
 
@@ -97,6 +110,7 @@ class TestViews(TestSetUp):
 
     # LOGOUT VIEW
     def test_doctor_can_logout_with_valid_token_authorized(self):
+        """Logout View: Doctor can logout when they have a valid token"""
         self.client.post(self.register_doctor_url, self.correct_doctor_data, format='json')
         res = self.client.post(self.login_doctor_url, self.correct_doctor_data['user'], format='json')
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + res.data['token'])
@@ -105,6 +119,7 @@ class TestViews(TestSetUp):
         self.assertEqual(res.status_code, 204)
 
     def test_doctor_cannot_logout_with_invalid_token_unauthorized(self):
+        """Logout View: Doctor cannot logout when they do not have a valid token"""
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + '123')
         res = self.client.post(self.logout_url)
 
--- backend/tests/test_accounts/test_patient_views.py
+++ backend/tests/test_accounts/test_patient_views.py
@@ -2,13 +2,15 @@ from .test_setup import TestSetUp
 from accounts.models import User
 
 class TestViews(TestSetUp):
-
+    """Test suite for views related to the Patient"""
     # REGISTER TESTS
     def test_patient_cannot_register_with_no_data(self):
+        """Registration: Patient cannot register with no data"""
         res = self.client.post(self.register_patient_url)
         self.assertEqual(res.status_code, 400)
 
     def test_patient_can_register_with_correct_data(self):
+        """Registration: Patient can register with correct data"""
         res = self.client.post(self.register_patient_url, self.correct_patient_data, format='json')
 
         self.assertTrue(User.objects.filter(email = self.correct_patient_data['user']['email']).exists())
@@ -16,32 +18,38 @@ class TestViews(TestSetUp):
         self.assertEqual(res.status_code, 201)
 
     def test_patient_cannot_register_with_no_user(self):
+        """Registration: Patient cannot register with no User (every Doctor contains a User)"""
         res = self.client.post(self.register_patient_url, self.no_user_patient_data, format='json')
 
         self.assertEqual(res.status_code, 400)
 
     def test_patient_cannot_register_with_no_first_name(self):
+        """Registration: Patient cannot register with no first_name attribute"""
         res = self.client.post(self.register_patient_url, self.no_first_name_patient_data, format='json')
 
         self.assertEqual(res.status_code, 400)
 
     def test_patient_cannot_register_with_no_email(self):
+        """Registration: Patient cannot register with no email attribute"""
         res = self.client.post(self.register_patient_url, self.no_email_patient_data, format='json')
 
         self.assertEqual(res.status_code, 400)
 
     def test_patient_cannot_register_with_no_password(self):
+        """Registration: Patient cannot register with no password attribute"""
         res = self.client.post(self.register_patient_url, self.no_password_patient_data, format='json')
 
         self.assertEqual(res.status_code, 400)
 
     def test_patient_cannot_register_with_invalid_email_format(self):
+        """Registration: Patient cannot register with an invalid email format"""
         self.correct_patient_data['user']['email'] = '123'
         res = self.client.post(self.register_patient_url, self.correct_patient_data, format='json')
 
         self.assertEqual(res.status_code, 400)
 
     def test_patient_cannot_register_with_existing_email(self):
+        """Registration: Patient cannot register with an existing email (emails are unique)"""
         self.client.post(self.register_patient_url, self.default_patient_data, format='json')
 
         self.correct_patient_data['user']['email'] = self.default_user_data['email']
@@ -66,18 +74,21 @@ class TestViews(TestSetUp):
 
     # LOGIN TESTS
     def test_patient_can_login_when_registered(self):
+        """Login: Patient can login once registered"""
         self.client.post(self.register_patient_url, self.correct_patient_data, format='json')
         res = self.client.post(self.login_patient_url, self.correct_patient_data['user'], format='json')
 
         self.assertEqual(res.status_code, 200)
 
     def test_patient_cannot_login_when_not_registered(self):
+        """Login: Patient cannot login if not registered"""
         res = self.client.post(self.login_patient_url, self.correct_patient_data['user'], format='json')
 
         self.assertEqual(res.status_code, 400)
 
     # USER VIEW
     def test_return_patient_valid_token_authorized(self):
+        """User View: Return a valid token for the Patient when authorized"""
         res = self.client.post(self.register_patient_url, self.correct_patient_data, format='json')
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + res.data['token'])
         res = self.client.get(self.users_patient_url)
@@ -85,11 +96,13 @@ class TestViews(TestSetUp):
         self.assertEqual(res.status_code, 200)
 
     def test_return_patient_no_token_unauthorized(self):
+        """User View: If no token for the Patient, we are unauthorized"""
         res = self.client.get(self.users_patient_url)
 
         self.assertEqual(res.status_code, 401)
 
     def test_return_patient_invalid_token_unauthorized(self):
+        """User View: Invalid token means Patient is unauthorized"""
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + '123')
         res = self.client.get(self.users_patient_url)
 
@@ -97,6 +110,7 @@ class TestViews(TestSetUp):
 
     # LOGOUT VIEW
     def test_patient_can_logout_with_valid_token_authorized(self):
+        """Logout View: Patient can logout when they have a valid token"""
         self.client.post(self.register_patient_url, self.correct_patient_data, format='json')
         res = self.client.post(self.login_patient_url, self.correct_patient_data['user'], format='json')
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + res.data['token'])
@@ -105,6 +119,7 @@ class TestViews(TestSetUp):
         self.assertEqual(res.status_code, 204)
 
     def test_patient_cannot_logout_with_invalid_token_unauthorized(self):
+        """Logout View: Doctor cannot logout when they do not have a valid token"""
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + '123')
         res = self.client.post(self.logout_url)
 
