84ca0d95a3213604da1305a929f6259cf4efd255
Peter Delis <delispeter19@gmail.com>
2022-02-20 00:07:17 -0500

    Add patient & doctor user accounts, tests ignored

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
-
+from django.utils.html import format_html
+from django.urls import reverse
+from .models import User, Doctor, Patient
 # Register your models here.
+
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "user_", "number_of_patients")
+    model = Doctor
+
+    empty_value_display = 'NO VALUE HERE :)'
+
+    def user_(self, obj):
+        link = reverse("admin:accounts_user_change", args=[obj.user.id]) #model name has to be lowercase
+        return format_html(u'<a href="%s">%s</a>' % (link, obj.user))
+
+    def number_of_patients(self, obj):
+        return len(Patient.objects.filter(doctor__user_id=obj.user_id))
+
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "user_", "doctor_")
+    list_filter = ("doctor",)
+
+    empty_value_display = 'NO VALUE HERE :)'
+
+    def user_(self, obj):
+        link = reverse("admin:accounts_user_change", args=[obj.user.id]) #model name has to be lowercase
+        return format_html(u'<a href="%s">%s</a>' % (link, obj.user))
+
+    def doctor_(self, obj):
+        if obj.doctor:
+            link = reverse("admin:accounts_doctor_change", args=[obj.doctor.user_id]) #model name has to be lowercase
+            return format_html(u'<a href="%s">%s</a>' % (link, obj.doctor))
+        else:
+            return None
+
+    # view_on_site = False
+
+admin.site.register(Doctor, DoctorAdmin)
+admin.site.register(User)
+admin.site.register(Patient, PatientAdmin)
--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,16 +1,25 @@
 """Router registration"""
 
-from unicodedata import name
+from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from accounts.views import *
 from knox import views as knox_views
+from .views import *
 
 app_name = "accounts"
 
+router = DefaultRouter()
+router.register(r"api/patients", DoctorPatientsView, basename="patients_of_doctor")
+
 urlpatterns = [
     path("api/auth", include("knox.urls")),
-    path("api/auth/register", RegisterView.as_view(), name='register'),
-    path("api/auth/login", LoginView.as_view(), name='login'),
+    path("api/auth/register/doctor", RegisterDoctorView.as_view(), name='register_doctor'),
+    path("api/auth/register/patient", RegisterPatientView.as_view(), name='register_patient'),
+    path("api/auth/login/doctor", LoginDoctorView.as_view(), name='login_doctor'),
+    path("api/auth/login/patient", LoginPatientView.as_view(), name='login_patient'),
+    path("api/auth/users/doctor", DoctorView.as_view(), name='users_doctor'),
+    path("api/auth/users/patient", PatientView.as_view(), name='users_patient'),
     path("api/auth/user", UserView.as_view(), name='user'),
     path("api/auth/logout", knox_views.LogoutView.as_view(), name="knox_logout"),
 ]
+
+urlpatterns += router.urls
