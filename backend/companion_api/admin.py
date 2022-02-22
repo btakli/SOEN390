43a0c4f21fff656f1d91e5529219f1d156df85de
Andrei Grosuliac <agrosuliac@gmail.com>
2022-02-22 15:13:35 -0500

    Added status model/views/serializers

--- backend/companion_api/admin.py
+++ backend/companion_api/admin.py
@@ -1,9 +1,14 @@
 """Register your models here"""
 
 from django.contrib import admin
-from .models import Person
+from .models import Person, Status
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ("__str__", 'date')
+    list_filter = ('patient',)
+    model = Status
+    search_fields = ("patient__first_name__startswith", ) #Search field (later can add different attributes?)
+    empty_value_display = 'NO VALUE HERE :)'
 
 admin.site.register(Person)
-
-if __name__ == "__main__":
-    print(admin.checks)
+admin.site.register(Status, StatusAdmin)
--- backend/companion_api/urls.py
+++ backend/companion_api/urls.py
@@ -3,10 +3,17 @@
 from rest_framework.routers import DefaultRouter
 from .models import *
 from .views import *
+from django.urls import path
 
 app_name = "companion_api"
 
 router = DefaultRouter()
 router.register(r"api/person", PersonView, basename="person")
+router.register(r"api/patient/status", PatientStatusView, basename="status")
+#router.register(r"api/patient/latest_status", LatestStatusView, basename="latest_status")
 
-urlpatterns = router.urls
+
+urlpatterns = [
+    path("api/patient/status/latest", LatestStatusView.as_view(), name='latest_status'), 
+]
+urlpatterns += router.urls
