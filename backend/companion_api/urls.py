c524fd50df0bc9ef9e720da8c7d3b47725fcbef5
Andrei Grosuliac <agrosuliac@gmail.com>
2022-02-23 17:22:29 -0500

    Added views for retrieving patients of doctor and a status for a specific person

--- backend/companion_api/urls.py
+++ backend/companion_api/urls.py
@@ -10,10 +10,12 @@ app_name = "companion_api"
 router = DefaultRouter()
 router.register(r"api/person", PersonView, basename="person")
 router.register(r"api/patient/status", PatientStatusView, basename="status")
+router.register(r"api/doctor/patients", DoctorPatientView, basename="patients")
 #router.register(r"api/patient/latest_status", LatestStatusView, basename="latest_status")
 
 
 urlpatterns = [
     path("api/patient/status/latest", LatestStatusView.as_view(), name='latest_status'), 
+    path("api/patient/status/latest/<int:pk>/", SpecificLatestStatusView.as_view(), name='specific_latest_status'), 
 ]
 urlpatterns += router.urls
--- backend/companion_api/views.py
+++ backend/companion_api/views.py
@@ -1,5 +1,6 @@
 """Create your views here"""
 
+from msilib.schema import Error
 from urllib import request
 from rest_framework import viewsets, permissions, generics
 from .models import *
@@ -23,6 +24,18 @@ class PersonView(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+class DoctorPatientView(viewsets.ModelViewSet):
+    """Patients View"""
+
+    # only authenticated users can see their patients
+    permission_classes = [permissions.IsAuthenticated]
+
+    serializer_class = DoctorSerializer
+
+    def get_queryset(self):
+        #doctorId = self.request.user.id
+        return self.request.user.doctor.patients.all()
+
 # Update the Patient's status view
 class PatientStatusView(viewsets.ModelViewSet):
     # only authenticated users can get access
@@ -35,6 +48,19 @@ class PatientStatusView(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(patient=self.request.user.patient)
+
+class SpecificLatestStatusView(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    serializer_class = StatusSerializer
+
+    def get_object(self):
+        patientID = self.kwargs['pk']
+        #print(patientID)
+        try:
+            return Status.objects.filter(patient__user_id = patientID).latest('date')
+        except:
+            pass
 
 class LatestStatusView(generics.RetrieveAPIView):
     permission_classes = [permissions.IsAuthenticated]
