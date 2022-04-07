d33c840c9afa6d0266b0387a52d925035f3b0713
Peter Delis <delispeter19@gmail.com>
2022-04-07 12:33:03 -0400

    Fix naming issue 1

--- frontend/src/pages/home/RequestHelp.js
+++ frontend/src/pages/home/RequestHelp.js
@@ -3,7 +3,6 @@ import React from "react";
 import RequestGroceryHelp from "../../pages/RequestHelpPage/RequestGroceryHelp";
 import RequestInformation from "../../pages/RequestHelpPage/RequestInformation";
 import RequestByInfected from "../../pages/RequestHelpPage/RequestByInfected";
-import Typography from "@mui/material/Typography";
 import { Card } from "@mui/material";
 
 function RequestHelp() {
