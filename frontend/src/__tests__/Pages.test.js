37256465cbd8caa90d1dff5f6dc7df7148abda5f
Matteo Gisondi <matteo.gisondi@gmail.com>
2022-04-07 00:05:43 -0400

    test: :test_tube:

--- frontend/src/__tests__/Pages.test.js
+++ frontend/src/__tests__/Pages.test.js
@@ -1,7 +1,7 @@
 // For testing props see https://testing-library.com/docs/react-testing-library/api/#rerender
 
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
 import store from "../redux/store";
