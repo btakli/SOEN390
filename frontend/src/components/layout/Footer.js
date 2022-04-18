2badaf9de65b0ea6642e1ad660bb1eb29c74c976
Peter Delis <delispeter19@gmail.com>
2022-04-18 14:34:46 -0400

    Update Footer

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -2,12 +2,13 @@ import React from "react";
 import Copyright from "../../components/layout/Copyright";
 
 // MUI
-import { Box } from "@mui/material";
+import { Box, Divider } from "@mui/material";
 
 function Footer() {
   return (
-    <Box component="footer" sx={{ p: 2, bgcolor: "#eaeff1" }}>
-        <Copyright />
+    <Box component="footer" sx={{ p: 2, bgcolor: "#eaeff1" }} >
+      <Divider sx={{"width": "100%", mb: 2}} />
+      <Copyright />
     </Box>
   );
 }
